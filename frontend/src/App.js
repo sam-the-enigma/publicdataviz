b79fffd930d7b40508adb92abb8f0248ad73613c
Samuel Smith <sam.smith@enigma.com>
2018-09-23 15:16:49 -0400

    Adding recharts for d3 charts in frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,20 +4,25 @@ import { PieChart, Pie } from 'recharts';
 
 class App extends Component {
 
+  componentWillMount() {
+    console.info('Hello')
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Public Data Viz</h1>
         </header>
-        <p className="App-intro">
+        <div className="App-intro">
           <PieChart width={100} height={100} className='pieChart'>
             <Pie
+              dataKey='value'
               data={[{name: 'Group A', value: 400}, {name: 'Group B', value: 300}]}
             >
             </Pie>
           </PieChart>
-        </p>
+        </div>
       </div>
     );
   }
